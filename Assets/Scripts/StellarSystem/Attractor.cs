ee745438871816ef7bb93f00cd5a52d7ac9f3835
Fabrice Chiron <fabrice.chiron@gmail.com>
2021-12-25 19:10:30 +0100

    Fix Attractor (for IsKinematic)

--- Assets/Scripts/StellarSystem/Attractor.cs
+++ Assets/Scripts/StellarSystem/Attractor.cs
@@ -30,13 +30,17 @@ public class Attractor : MonoBehaviour
     {
         Rigidbody rbToAttract = objToAttract.rb;
 
-        Vector3 direction = rb.position - rbToAttract.position;
-        float distance = direction.magnitude;
+        if (!rbToAttract.isKinematic)
+        {
+            Vector3 direction = rb.position - rbToAttract.position;
+            float distance = direction.magnitude;
 
-        float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
+            float forceMagnitude = G * (rb.mass * rbToAttract.mass) / Mathf.Pow(distance, 2);
 
-        Vector3 force = direction.normalized * forceMagnitude;
+            Vector3 force = direction.normalized * forceMagnitude;
+            Debug.Log($"{rb.name} attracts {rbToAttract.name}");
+            rbToAttract.AddForce(force);
+        }
 
-        rbToAttract.AddForce(force);
     }
 }
